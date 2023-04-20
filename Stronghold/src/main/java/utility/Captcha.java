98bc541e93a7d2c9631aac214d1df3aed2ce1299
sahand-es <844sahand@gmail.com>
2023-04-20 17:04:50 +0430

    captcha: Captcha: Reset and check added

--- Stronghold/src/main/java/utility/Captcha.java
+++ Stronghold/src/main/java/utility/Captcha.java
@@ -2,22 +2,29 @@ package utility;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.Scanner;
 
 public class Captcha {
-private final String captchaString;
+    private String captchaString;
 
     public Captcha() {
         captchaString = RandomGenerators.randomCaptcha();
     }
 
+    public boolean checkCaptcha(String input) {
+        return input.equals(captchaString);
+    }
 
-    public String getCaptcha()
-    {
+    public String getCaptcha() {
         return makeAsciiArt();
     }
 
-    public String addNoise(String asciiArt, int noisePercentage)
-    {
+    public String resetAndGetCaptcha() {
+        captchaString = RandomGenerators.randomCaptcha();
+        return getCaptcha();
+    }
+
+    public String addNoise(String asciiArt, int noisePercentage) {
         StringBuilder str = new StringBuilder(asciiArt);
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == ' ')
@@ -28,9 +35,8 @@ private final String captchaString;
         return str.toString();
     }
 
-    private String makeAsciiArt()
-    {
-        int width = captchaString.length()* 11;
+    private String makeAsciiArt() {
+        int width = captchaString.length() * 11;
         int height = 11;
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         Graphics graphics1 = image.getGraphics();
@@ -42,21 +48,13 @@ private final String captchaString;
 
         StringBuilder sb = new StringBuilder();
 
-        for (int y = 0; y < height ; y++) {
+        for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++)
                 sb.append(image.getRGB(x, y) == -16777216 ? " " : "$");
             if (sb.toString().trim().isEmpty()) continue;
             if (y != height - 1) sb.append("\n");
         }
-       return addNoise(sb.toString(), 25);
+        return addNoise(sb.toString(), 25);
     }
-
-    public static void main(String[] args) {
-        Captcha captcha = new Captcha();
-        System.out.println(captcha.getCaptcha());
-        System.out.println(captcha.captchaString);
-//        System.out.println(RandomGenerators.randomSpecialCharacter());
-    }
-
 
 }
