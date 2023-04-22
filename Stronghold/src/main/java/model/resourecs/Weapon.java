c49875aedeaf4760153ce5b3d81115624414135a
sahand-es <844sahand@gmail.com>
2023-04-22 11:05:51 +0430

    model: Add resources enum

--- Stronghold/src/main/java/model/resourecs/Weapon.java
+++ Stronghold/src/main/java/model/resourecs/Weapon.java
@@ -3,6 +3,7 @@ package model.resourecs;
 import model.society.enums.Resources;
 
 public class Weapon extends Resource{
+
     public Weapon(Resources resource) {
         super(resource);
     }
--- Stronghold/src/main/java/model/society/enums/Resources.java
+++ Stronghold/src/main/java/model/society/enums/Resources.java
@@ -1,6 +1,54 @@
 package model.society.enums;
 
+import model.resourecs.Resource;
+
 public enum Resources
 {
-//    this includes foods.
+//  Initial materials:
+    WHEAT("Wheat"),
+    FLOUR("Flour"),
+    HOPS("Hops"),
+    ALE("Ale"),
+    STONE("Stone"),
+    IRON("Iron"),
+    WOOD("Wood"),
+    PITCH("Pitch"),
+
+//  Foods:
+    MEAT("Meat"),
+    APPLE("Apple"),
+    CHEESE("Cheese"),
+    BREAD("Bread"),
+
+//  Weapons:
+    BOW("Bow"),
+    CROSSBOW("Crossbow"),
+    SPEAR("Spear"),
+    PIKE("Pike"),
+    MACE("Mace"),
+    SWORD("Sword"),
+
+//  Armors:
+    LEATHER("Leather"),
+    METAL("Metal"),
+    HORSE("Horse"),
+
+//  Others:
+    COW("Cow");
+
+
+    private final String name;
+
+    Resources(String name) {
+        this.name = name;
+    }
+
+    public Resources getResourceByName(String resourceName)
+    {
+        for (Resources resource : Resources.values()) {
+            if (resource.name.equalsIgnoreCase(resourceName.trim()))
+                return resource;
+        }
+        return null;
+    }
 }
