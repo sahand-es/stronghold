8f28d4e9fa4feae196cefd1707ad52ca2d7cfd85
sahand-es <844sahand@gmail.com>
2023-04-22 09:56:25 +0430

    signup-menu: DataManager: Add TODO

--- Stronghold/src/main/java/utility/DataManager.java
+++ Stronghold/src/main/java/utility/DataManager.java
@@ -32,6 +32,11 @@ public class DataManager {
         }
     }
 
+    /**
+     * call when you want save users database
+     */
+
+    // TODO: 4/22/2023  call when program ends
     public static void saveUsers() {
         FileWriter fileWriter;
         try {
