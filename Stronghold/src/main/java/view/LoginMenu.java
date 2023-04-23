e86f2ceebde3dfd80cf8dc31d9fffecd40cc94e3
Yousef61583 <118717370+Yousef61583@users.noreply.github.com>
2023-04-23 19:33:43 +0430

    login-menu: LoginMenu: start making run method.

--- Stronghold/src/main/java/view/LoginMenu.java
+++ Stronghold/src/main/java/view/LoginMenu.java
@@ -3,16 +3,18 @@ package view;
 import java.util.regex.Matcher;
 public class LoginMenu
 {
-    public static void run()
-    {
+    public static void run() {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        String command;
+        Matcher matcher;
 
     }
 
-    private static void checkLogin(Matcher matcher){
+    private static void checkLogin(Matcher matcher) {
 
     }
 
-    private static void checkForgotPassword(){
+    private static void checkForgotPassword() {
 
     }
 
