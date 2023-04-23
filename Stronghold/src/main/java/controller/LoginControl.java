6a387a19b1ce86e4ee833935b192692df8668e75
Yousef61583 <118717370+Yousef61583@users.noreply.github.com>
2023-04-23 21:14:02 +0430

    login-menu: LoginControl: checkLogin added.

--- Stronghold/src/main/java/controller/LoginControl.java
+++ Stronghold/src/main/java/controller/LoginControl.java
@@ -1,11 +1,31 @@
 package controller;
 
+import model.Application;
+import model.User;
 import view.enums.messages.LoginMessages;
 
-public class LoginControl
-{
+public class LoginControl {
     public static LoginMessages checkLogin(String username, String password){
-        return null;
+        User user;
+
+        if (username == null)
+            return LoginMessages.EMPTY_USERNAME;
+
+        if (password == null)
+            return LoginMessages.EMPTY_PASSWORD;
+
+        user = Application.getUserByUsername(username);
+
+        if (user == null)
+            return LoginMessages.USERNAME_DIDNT_MATCH;
+
+        if (!user.checkPassword(password))
+            return LoginMessages.PASSWORD_DIDNT_MATCH;
+
+
+
+
+        return LoginMessages.SUCCESSFUL;
     }
 
     public static LoginMessages checkForgotPassword(String answer){
