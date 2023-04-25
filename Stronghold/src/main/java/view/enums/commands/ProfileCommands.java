9a51d3eb3e7dd6b7c27bdd3e61064c89cda105e8
Yousef61583 <118717370+Yousef61583@users.noreply.github.com>
2023-04-25 23:04:11 +0430

    profile-menu: ProfileCommands: commands added.

--- Stronghold/src/main/java/view/enums/commands/ProfileCommands.java
+++ Stronghold/src/main/java/view/enums/commands/ProfileCommands.java
@@ -3,8 +3,19 @@ package view.enums.commands;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public enum ProfileCommands
-{
+public enum ProfileCommands {
+
+
+    CHANGE_USERNAME("^profile change -u (?<username>.+)$"),
+    CHANGE_NICKNAME("^profile change -n (?<nickname>.+)$"),
+    CHANGE_PASSWORD("^profile change password -o (?<oldPassword>\\S+) -n (?<newPassword>\\S+)$"),
+    CHANGE_EMAIL("^profile change -e (?<email>\\S+)$"),
+    CHANGE_SLOGAN("^profile change slogan -s (?<slogan>.+)$"),
+    REMOVE_SLOGAN("^profile remove slogan$"),
+    DISPLAY_HIGH_SCORE("^profile display highscore$"),
+    DISPLAY_RANK("^profile display rank$"),
+    DISPLAY_SLOGAN("^profile display slogan$"),
+    DISPLAY_ALL("^profile display$")
     ;
 
     String regex;
