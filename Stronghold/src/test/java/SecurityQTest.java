888e388adb88c05de49ae733ab14b8e738fcbbbe
ArminGer03 <a1382.geramirad@gmail.com>
2023-05-04 17:27:05 +0330

    Added methods to SecurityQTest class

--- Stronghold/src/test/java/SecurityQTest.java
+++ Stronghold/src/test/java/SecurityQTest.java
@@ -1,2 +1,40 @@
+import org.junit.jupiter.api.Test;
+import view.SignUpMenu;
+import view.enums.commands.SignUpCommands;
+
+import java.util.HashMap;
+import java.util.regex.Matcher;
+
+import static org.junit.jupiter.api.Assertions.*;
 public class SecurityQTest {
+
+    public void testNull(String input) {
+        HashMap<String, String> data = SignUpMenu.extractSecurityAnswer(input);
+        assertNull(data, "It should return null");
+    }
+
+
+    public void testExtraction(HashMap<String, String> answer, String input) {
+        HashMap<String, String> data = SignUpMenu.extractSecurityAnswer(input);
+        assertTrue(isEqual(answer, data));
+    }
+
+    private boolean isEqual(HashMap<String, String> answer, HashMap<String, String> data) {
+        for (String s : answer.keySet()) {
+            if (!answer.get(s).equals(data.get(s)))
+                return false;
+        }
+
+        return true;
+    }
+
+    private HashMap<String, String> answerMap(String questionNumber, String answer, String answerConfirm) {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("questionNumber", questionNumber);
+        data.put("answer", answer);
+        data.put("answerConfirm", answerConfirm);
+
+        return data;
+    }
+
 }
