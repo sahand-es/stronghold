e2e1a1a4f3de2cac7a6f178064459e920088b5a7
sahand-es <844sahand@gmail.com>
2023-05-06 14:05:39 +0430

    building: ResourceMakerBuilding: Constructor added

--- Stronghold/src/main/java/model/environment/buldings/ResourceMakerBuilding.java
+++ Stronghold/src/main/java/model/environment/buldings/ResourceMakerBuilding.java
@@ -2,18 +2,22 @@ package model.environment.buldings;
 
 import model.environment.buldings.enums.BuildingCategory;
 import model.resourecs.Resource;
+import model.resourecs.ResourcesName;
 import model.society.Government;
 import model.units.Person;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ResourceMakerBuilding extends ResourceExtractorBuilding {
 
     ArrayList<Resource> inputResource;
 
-    public ResourceMakerBuilding(String name, int size, BuildingCategory category, int hp, Government owner, ArrayList<Resource> price, ArrayList<Resource> extractedResource, int rate, ArrayList<Person> workers, ArrayList<Resource> inputResource) {
-        super(name,size,category, hp, owner, price, extractedResource, rate,workers);
-        this.inputResource = inputResource;
+    protected ResourceMakerBuilding(int size,
+                                    int hp,
+                                    BuildingCategory category,
+                                    HashMap<ResourcesName, Integer> price) {
+        super(size, hp, category, price);
     }
 
     public ArrayList<Resource> getInputResource() {
