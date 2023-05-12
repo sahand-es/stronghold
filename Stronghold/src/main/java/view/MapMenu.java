1a130032fe35cc9ce3b7d41eccfb84202583c21a
ArminGer03 <a1382.geramirad@gmail.com>
2023-05-12 23:13:32 +0330

    MapMenu: Added change coordinate

--- Stronghold/src/main/java/view/MapMenu.java
+++ Stronghold/src/main/java/view/MapMenu.java
@@ -1,19 +1,45 @@
 package view;
 
 import model.Application;
+import utility.CheckFunctions;
 import utility.DataManager;
 import view.enums.AllMenus;
+import view.enums.commands.LoginCommands;
 import view.enums.commands.MainMenuCommands;
 import view.enums.commands.MapMenuCommands;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MapMenu {
+    private static int mapX,mapY;
+
+    public static int getMapX() {
+        return mapX;
+    }
+
+    public static void setMapX(int mapX) {
+        MapMenu.mapX = mapX;
+    }
+
+    public static int getMapY() {
+        return mapY;
+    }
+
+    public static void setMapY(int mapY) {
+        MapMenu.mapY = mapY;
+    }
+
     public static void run(int x , int y){
+        setMapX(x);
+        setMapY(y);
 
         java.util.Scanner scanner = new java.util.Scanner(System.in);
         String command;
 
         while(true){
 
+            System.out.println("XY: " + getMapX() + " " + getMapY());
             //todo literally show the fucking map!
 
             command = scanner.nextLine();
@@ -23,14 +49,15 @@ public class MapMenu {
                 Application.setCurrentMenu(AllMenus.MAIN_MENU);
                 return;
             }
-            //todo get direction
+            else if(MapMenuCommands.getMatcher(command,MapMenuCommands.MAP) != null){
+                getCoordinates(command);
+            }
             //todo show detail
-            else if(MainMenuCommands.getMatcher(command,MainMenuCommands.SHOW_MENU) != null){
+            else if(MapMenuCommands.getMatcher(command,MapMenuCommands.SHOW_MENU) != null){
                 System.out.println("You're in Map Menu!");
             }
             else
                 System.out.println("My liege, that's an invalid command!");
-
 
 
             switch (Application.getCurrentMenu()){
@@ -41,6 +68,55 @@ public class MapMenu {
             }
         }
 
+    }
+
+
+    private static void getCoordinates(String command){
+        int right = 0, left = 0, up = 0, down = 0;
+
+        String regexRight = MapMenuCommands.getRegexRight();
+        Matcher rightMatcher = Pattern.compile(regexRight).matcher(command);
+        while (rightMatcher.find()){
+            String temp = rightMatcher.group("rightNumber");
+            temp = (temp == null) ? String.valueOf(1) : temp;
+            right += Integer.parseInt(temp);
+            command = command.replaceFirst(rightMatcher.group().toString(),"");
+        }
+
+        String regexLeft = MapMenuCommands.getRegexLeft();
+        Matcher leftMatcher = Pattern.compile(regexLeft).matcher(command);
+        while (leftMatcher.find()){
+            String temp = leftMatcher.group("leftNumber");
+            temp = (temp == null) ? String.valueOf(1) : temp;
+            left += Integer.parseInt(temp);
+            command = command.replaceFirst(leftMatcher.group().toString().trim(),"");
+        }
+
+        String regexUp = MapMenuCommands.getRegexUP();
+        Matcher upMatcher = Pattern.compile(regexUp).matcher(command);
+        while (upMatcher.find()){
+            String temp = upMatcher.group("upNumber");
+            temp = (temp == null) ? String.valueOf(1) : temp;
+            up += Integer.parseInt(temp);
+            command = command.replaceFirst(upMatcher.group().toString().trim(),"");
+        }
+
+        String regexDown = MapMenuCommands.getRegexDown();
+        Matcher downMatcher = Pattern.compile(regexDown).matcher(command);
+        while (downMatcher.find()){
+            String temp = downMatcher.group("downNumber");
+            temp = (temp == null) ? String.valueOf(1) : temp;
+            down += Integer.parseInt(temp);
+            command = command.replaceFirst(downMatcher.group().toString().trim(),"");
+        }
+
+        if (MapMenuCommands.getMatcher(command,MapMenuCommands.MAP_FINAL_CHECK) == null){
+            System.out.println("Invalid argument in direction command!");
+        }
+        else{
+            setMapX(getMapX() + right - left);
+            setMapY(getMapY() + up - down);
+        }
     }
 
 }
--- Stronghold/src/main/java/view/enums/commands/MapMenuCommands.java
+++ Stronghold/src/main/java/view/enums/commands/MapMenuCommands.java
@@ -6,7 +6,12 @@ import java.util.regex.Pattern;
 public enum MapMenuCommands {
     SHOW_MENU("^\\s*show\\s+menu\\s*$"),
     EXIT("^\\s*exit\\s*$"),
-
+    MAP("^\\s*map\\s.+"),
+    MAP_FINAL_CHECK("^\\s*map(\\s+)?"),
+    RIGHT("(?<rightNumber>\\d+)?(\\s+)?right"),
+    LEFT("(?<leftNumber>\\d+)?(\\s+)?left"),
+    UP("(?<upNumber>\\d+)?(\\s+)?up"),
+    DOWN("(?<downNumber>\\d+)?(\\s+)?down"),
     ;
 
     String regex;
@@ -20,5 +25,18 @@ public enum MapMenuCommands {
     {
         Matcher matcher = Pattern.compile(command.regex).matcher(input);
         return matcher.matches() ? matcher : null;
+    }
+
+    public static String getRegexRight(){
+        return RIGHT.regex;
+    }
+    public static String getRegexLeft(){
+        return LEFT.regex;
+    }
+    public static String getRegexUP(){
+        return UP.regex;
+    }
+    public static String getRegexDown(){
+        return DOWN.regex;
     }
 }
