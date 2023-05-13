d12f9ac6ed06d97d99390773510941ea9a3d27f0
ArminGer03 <a1382.geramirad@gmail.com>
2023-05-13 07:09:28 +0330

    Made TradeMenu

--- Stronghold/src/main/java/view/TradeMenu.java
+++ Stronghold/src/main/java/view/TradeMenu.java
@@ -1,11 +1,43 @@
 package view;
 
-public class TradeMenu
-{
+import model.Application;
+import model.map.Block;
+import view.enums.AllMenus;
+import view.enums.commands.MapMenuCommands;
+
+public class TradeMenu {
+    public static void run() {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        String command;
+        while(true){
+
+            command = scanner.nextLine();
+
+//            if(MapMenuCommands.getMatcher(command, MapMenuCommands.) != null){
+//                System.out.println("You're in Main Menu!");
+//                Application.setCurrentMenu(AllMenus.MAIN_MENU);
+//                return;
+//            }
+//            else if(MapMenuCommands.getMatcher(command,MapMenuCommands.MAP) != null){
+//                getCoordinates(command);
+//            }
+//            else if(MapMenuCommands.getMatcher(command,MapMenuCommands.SHOW_DETAILS) != null){
+//                checkShowMap(command);
+//            }
+//            else if(MapMenuCommands.getMatcher(command,MapMenuCommands.SHOW_MENU) != null){
+//                System.out.println("You're in Map Menu!");
+//            }
+//            else
+//                System.out.println("My liege, that's an invalid command!");
 
 
-    public void run()
-    {
+//            switch (Application.getCurrentMenu()){
+//                case MAIN_MENU:
+//                    return;
+//                default:
+//                    break;
+//            }
+        }
 
     }
 }
