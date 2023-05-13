5d73a973977a548a2009ac3295a3ed2b66656aba
ArminGer03 <a1382.geramirad@gmail.com>
2023-05-13 14:11:07 +0330

    Get input command up to Repair command

--- Stronghold/src/main/java/view/GameMenu.java
+++ Stronghold/src/main/java/view/GameMenu.java
@@ -2,40 +2,214 @@ package view;
 
 import model.Application;
 import model.Game;
+import model.map.Block;
+import model.map.Map;
 import utility.DataManager;
 import view.enums.AllMenus;
 import view.enums.commands.GameCommands;
 import view.enums.commands.MainMenuCommands;
+import view.enums.commands.MapMenuCommands;
 
-public class GameMenu
-{
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class GameMenu {
+    private static int HEIGHT = 200;
+    private static int WIDTH = 200;
+
     public static void run() {
+        Application.setCurrentMenu(AllMenus.GAME_MENU);
         java.util.Scanner scanner = new java.util.Scanner(System.in);
         String command;
 
-        command = scanner.nextLine();
-        if(GameCommands.getMatcher(command,GameCommands.BACK) != null){
-            System.out.println("You're now in Main Menu!");
-            Application.setCurrentMenu(AllMenus.MAIN_MENU);
+        while (true){
+            command = scanner.nextLine();
+            if(GameCommands.getMatcher(command,GameCommands.BACK) != null){
+                System.out.println("You're now in Main Menu!");
+                Application.setCurrentMenu(AllMenus.MAIN_MENU);
+                return;
+            }
+            else if(GameCommands.getMatcher(command,GameCommands.SHOW_MENU) != null){
+                System.out.println("You're now in Game Menu!");
+            }
+            else if(GameCommands.getMatcher(command,GameCommands.TRADE) != null){
+                Application.setCurrentMenu(AllMenus.TRADE_MENU);
+            }
+            else if(GameCommands.getMatcher(command,GameCommands.SHOW_POPULARITY_FACTORS) != null){
+                //todo show popularity factors
+            }
+            else if(GameCommands.getMatcher(command,GameCommands.SHOW_POPULARITY) != null){
+                //todo show popularity
+            }
+            else if(GameCommands.getMatcher(command,GameCommands.SHOW_FOOD) != null){
+                //todo show food list
+            }
+            else if(GameCommands.getMatcher(command,GameCommands.FOOD_RATE) != null){
+                //todo get food rate
+            }
+            else if(GameCommands.getMatcher(command,GameCommands.FOOD_RATE_SHOW) != null){
+                //todo show food rate
+            }
+            else if(GameCommands.getMatcher(command,GameCommands.TAX_RATE) != null){
+                //todo get tax rate
+            }
+            else if(GameCommands.getMatcher(command,GameCommands.TAX_RATE_SHOW) != null){
+                //todo show tax rate
+            }
+            else if(GameCommands.getMatcher(command,GameCommands.FEAR_RATE) != null){
+                //todo get tax rate
+            }
+            else if(GameCommands.getMatcher(command,GameCommands.FEAR_RATE_SHOW) != null){
+                //todo show tax rate
+            }
+            else if(GameCommands.getMatcher(command,GameCommands.CREATE_BUILDING) != null){
+                checkCreateBuilding(command);
+            }
+            else if(GameCommands.getMatcher(command,GameCommands.SELECT_BUILDING) != null){
+                checkSelectBuilding(command);
+            }
+            else if(GameCommands.getMatcher(command,GameCommands.CREATE_UNIT) != null){
+                checkCreateUnit(command);
+            }
+            else if(GameCommands.getMatcher(command,GameCommands.REPAIR) != null){
+                //todo repair
+            }
+            else
+                System.out.println("My liege, that's an invalid command!");
+
+
+            switch (Application.getCurrentMenu()){
+                case TRADE_MENU:
+                    System.out.println("You're now in Trade Menu!");
+                    TradeMenu.run();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private static void checkCreateBuilding(String command){
+        String x,y,type;
+
+        String regexForX = GameCommands.getRegexForX();
+        Matcher matcherX = Pattern.compile(regexForX).matcher(command);
+        if(!matcherX.find()){
+            System.out.println("You must give x!");
             return;
         }
-        else if(GameCommands.getMatcher(command,GameCommands.SHOW_MENU) != null){
-            System.out.println("You're now in Game Menu!");
-        }
-        else if(GameCommands.getMatcher(command,GameCommands.TRADE) != null){
-            Application.setCurrentMenu(AllMenus.TRADE_MENU);
+        else
+            x = matcherX.group("xNum");
+
+        command = command.replaceAll(matcherX.group().toString().trim(),"");
+
+        String regexForY = GameCommands.getRegexForY();
+        Matcher matcherY = Pattern.compile(regexForY).matcher(command);
+        if(!matcherY.find()){
+            System.out.println("You must give y!");
+            return;
         }
         else
-            System.out.println("My liege, that's an invalid command!");
+            y = matcherY.group("yNum");
 
+        command = command.replaceAll(matcherY.group().toString().trim(),"");
 
-        switch (Application.getCurrentMenu()){
-        case TRADE_MENU:
-            System.out.println("You're now in Trade Menu!");
-            TradeMenu.run();
-            break;
-        default:
-            break;
+        String regexForType = GameCommands.getRegexForType();
+        Matcher matcherType = Pattern.compile(regexForType).matcher(command);
+        if(!matcherType.find()){
+            System.out.println("You must give type!");
+            return;
         }
+        else
+            type = matcherType.group("type");
+
+        command = command.replaceAll(matcherType.group().toString().trim(),"");
+
+        if (GameCommands.getMatcher(command, GameCommands.CREATE_BUILDING_CHECK) == null){
+            System.out.println("Invalid argument in create building command!");
+            return;
+        }
+
+
+        //todo give this to it's controller
+
+    }
+
+    private static void checkSelectBuilding(String command){
+        String x,y;
+
+        String regexForX = GameCommands.getRegexForX();
+        Matcher matcherX = Pattern.compile(regexForX).matcher(command);
+        if(!matcherX.find()){
+            System.out.println("You must give x!");
+            return;
+        }
+        else
+            x = matcherX.group("xNum");
+
+        command = command.replaceAll(matcherX.group().toString().trim(),"");
+
+        String regexForY = GameCommands.getRegexForY();
+        Matcher matcherY = Pattern.compile(regexForY).matcher(command);
+        if(!matcherY.find()){
+            System.out.println("You must give y!");
+            return;
+        }
+        else
+            y = matcherY.group("yNum");
+
+        command = command.replaceAll(matcherY.group().toString().trim(),"");
+
+
+        if (GameCommands.getMatcher(command, GameCommands.SELECT_BUILDING_CHECK) == null){
+            System.out.println("Invalid argument in select building command!");
+            return;
+        }
+
+
+        //todo give this to it's controller
+
+    }
+
+    private static void checkCreateUnit(String command){
+        String type,count;
+
+        String regexForType = GameCommands.getRegexForType();
+        Matcher matcherType = Pattern.compile(regexForType).matcher(command);
+        if(!matcherType.find()){
+            System.out.println("You must give type!");
+            return;
+        }
+        else
+            type = matcherType.group("type");
+
+        command = command.replaceAll(matcherType.group().toString().trim(),"");
+
+        String regexForCount = GameCommands.getRegexForCount();
+        Matcher matcherCount = Pattern.compile(regexForCount).matcher(command);
+        if(!matcherCount.find()){
+            System.out.println("You must give count!");
+            return;
+        }
+        else
+            count = matcherCount.group("count");
+
+        command = command.replaceAll(matcherCount.group().toString().trim(),"");
+
+
+        if (GameCommands.getMatcher(command, GameCommands.CREATE_UNIT_CHECK) == null){
+            System.out.println("Invalid argument in create unit command!");
+            return;
+        }
+
+
+        //todo give this to it's controller
+
+    }
+
+
+    public static void main(String[] args) {
+        GameMenu gameMenu = new GameMenu();
+        gameMenu.run();
     }
 }
--- Stronghold/src/main/java/view/enums/commands/GameCommands.java
+++ Stronghold/src/main/java/view/enums/commands/GameCommands.java
@@ -6,6 +6,26 @@ import java.util.regex.Pattern;
 public enum GameCommands {
     BACK("^\\s*back\\s*$"),
     SHOW_MENU("^\\s*show\\s+menu\\s*$"),
+    SHOW_POPULARITY("^\\s*show\\s+popularity\\s*$"),
+    SHOW_POPULARITY_FACTORS("^\\s*show\\s+popularity\\s+factors\\s*$"),
+    SHOW_FOOD("^\\s*show\\s+food\\s+list\\s*$"),
+    FOOD_RATE("^\\s*food\\s+rate\\s+-r\\s+(?<rateNumber>\\d+)\\s*$"),
+    FOOD_RATE_SHOW("^\\s*food\\s+rate\\s+show\\s*$"),
+    TAX_RATE("^\\s*tax\\s+rate\\s+-r\\s+(?<taxNumber>\\d+)\\s*$"),
+    TAX_RATE_SHOW("^\\s*tax\\s+rate\\s+show\\s*$"),
+    FEAR_RATE("^\\s*fear\\s+rate\\s+-r\\s+(?<fearNumber>\\d+)\\s*$"),
+    FEAR_RATE_SHOW("^\\s*fear\\s+rate\\s+show\\s*$"),
+    CREATE_BUILDING("^\\s*create\\s+building\\s+.*"),
+    CREATE_BUILDING_CHECK("^\\s*create\\s+building\\s+$"),
+    SELECT_BUILDING("^\\s*select\\s+building\\s+.*"),
+    SELECT_BUILDING_CHECK("^\\s*select\\s+building\\s+$"),
+    CREATE_UNIT("^\\s*createunit\\s+.*"),
+    CREATE_UNIT_CHECK("^\\s*createunit\\s+$"),
+    X_DIR("-x\\s+(?<xNum>\\d+)"),
+    Y_DIR("-y\\s+(?<yNum>\\d+)"),
+    TYPE("-t\\s+(?<type>\\S+)"),
+    COUNT("-c\\s+(?<count>\\d+)"),
+    REPAIR("^\\s*repair\\s*$"),
     TRADE("^\\s*trade\\s+menu\\s*$")
     ;
 
@@ -19,5 +39,18 @@ public enum GameCommands {
     public static Matcher getMatcher(String input , GameCommands command) {
         Matcher matcher = Pattern.compile(command.regex).matcher(input);
         return matcher.matches() ? matcher : null;
+    }
+
+    public static String getRegexForX(){
+        return X_DIR.regex;
+    }
+    public static String getRegexForY(){
+        return Y_DIR.regex;
+    }
+    public static String getRegexForType(){
+        return TYPE.regex;
+    }
+    public static String getRegexForCount(){
+        return COUNT.regex;
     }
 }
