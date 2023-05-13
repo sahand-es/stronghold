c084171cb6ac41bae774369637c3c623f0145953
ArminGer03 <a1382.geramirad@gmail.com>
2023-05-13 07:11:45 +0330

    Made TradeCommands Enum

--- Stronghold/src/main/java/view/enums/commands/TradeCommands.java
+++ Stronghold/src/main/java/view/enums/commands/TradeCommands.java
@@ -1,5 +1,23 @@
 package view.enums.commands;
 
-public enum TradeCommands
-{
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public enum TradeCommands {
+    BACK("^\\s*back\\s*$"),
+    SHOW_MENU("^\\s*show\\s+menu\\s*$"),
+    ;
+
+    String regex;
+
+    TradeCommands(String regex)
+    {
+        this.regex = regex;
+    }
+
+    public static Matcher getMatcher(String input , TradeCommands command) {
+        Matcher matcher = Pattern.compile(command.regex).matcher(input);
+        return matcher.matches() ? matcher : null;
+    }
+    
 }
