eb6f521650e2b43b409a9c67987048b63c77f9e0
Yousef61583 <118717370+Yousef61583@users.noreply.github.com>
2023-05-13 18:12:59 +0330

    game: GameMessages: messages added

--- Stronghold/src/main/java/view/enums/messages/GameMessages.java
+++ Stronghold/src/main/java/view/enums/messages/GameMessages.java
@@ -1,53 +1,62 @@
 package view.enums.messages;
 
 public enum GameMessages {
-    INVALID_RATE,
-    INVALID_XY,
-    NOT_ENOUGH_RESOURCE,
+    INVALID_RATE("Rate is invalid"),
+    INVALID_XY("Invalid Coordinates"),
+    NOT_ENOUGH_RESOURCE("Not enough resource"),
 // create building:
-    CANNOT_BUILD_HERE,
+    CANNOT_BUILD_HERE("Can not build on this block"),
 // select building:
-    INVALID_BUILDING_TYPE,
-    EMPTY_XY_BUILDING,
-    NOT_YOURS_BUILDING,
-    NOT_A_BUILDING,
+    INVALID_BUILDING_TYPE("There is no building with this name!"),
+    EMPTY_XY_BUILDING("There is no building on this block!"),
+    NOT_YOURS_BUILDING("This building doesn't belong to you"),
+    NOT_A_BUILDING("You can not select this Environment"),
 // create unit:
-    BUILDING_NOT_SELECTED,
-    INVALID_UNIT_TYPE,
-    INVALID_COUNT,
-    CANNOT_MAKE_THIS_UNIT_IN_THIS_BUILDING,
-    NOT_UNIT_MAKER,
+    BUILDING_NOT_SELECTED("You should select a Building"),
+    INVALID_UNIT_TYPE("There is no unit with this name!"),
+    INVALID_COUNT("Unit count is Invalid"),
+    CANNOT_MAKE_THIS_UNIT_IN_THIS_BUILDING("you cant make this unit in this building"),
+    NOT_UNIT_MAKER("You should select a UnitMaker Building"),
 // repair:
-    NOT_ENOUGH_ROCK,
-    ENEMY_CLOSE,
-    CANNOT_REPAIR_THIS_BUILDING_TYPE,
+    NOT_ENOUGH_ROCK("You doesn't have enough stones to repair this building"),
+    ENEMY_CLOSE("Cannot repair: enemy units are to close"),
+    CANNOT_REPAIR_THIS_BUILDING_TYPE("Cannot repair: this building is not a castle type building"),
 // select unit:
-    EMPTY_XY_UNIT,
-    NOT_YOURS_UNIT,
+    EMPTY_XY_UNIT("There is no unit on this coordinates"),
+    NOT_YOURS_UNIT("This unit does not belong to you"),
 // move unit:
-    CANNOT_GO_THERE,
+    CANNOT_GO_THERE("Unit cant go there"),
 // patrol unit:
-    INVALID_UNIT_STATE,
-    CLOSE_RANGE_UNIT,
-    UNIT_NOT_SELECTED,
-    THIS_UNIT_DOES_NOT_HAVE_STATE,
-    SUCCESS,
+    INVALID_UNIT_STATE("Invalid Unit state"),
+
+    UNIT_NOT_SELECTED("You have not selected a unit"),
+    THIS_UNIT_DOES_NOT_HAVE_STATE("This unit doesnt have state"),
+    SUCCESS("success"),
 //  attack:
-    CAN_NOT_ATTACK_WITH_THIS_UNIT_TYPE,
-    NO_ENEMY_TO_ATTACK,
-    CAN_NOT_GO_THERE_TO_ATTACK,
-    TRADE_MENU,
+    CAN_NOT_ATTACK_WITH_THIS_UNIT_TYPE("This unit cannot attack"),
+    NO_ENEMY_TO_ATTACK("There is no enemy to attack"),
+    CAN_NOT_GO_THERE_TO_ATTACK("Unit cant go there"),
+    TRADE_MENU("trade menu"),
 //  pour oil:
-    SELECTED_UNIT_IS_NOT_ENGINEER,
-    INVALID_DIRECTION,
+    SELECTED_UNIT_IS_NOT_ENGINEER("This unit is not an engineer"),
+    INVALID_DIRECTION("Invalid Direction"),
 
 //  dig tunnel
 
-    SELECTED_UNIT_IS_NOT_TUNNELER,
+    SELECTED_UNIT_IS_NOT_TUNNELER("This unit is not a tunneler"),
 
-    CAN_NOT_DIG_THERE;
+    CAN_NOT_DIG_THERE("can not dig this block");
     ;
 
+    final String message;
+
+    GameMessages(String message){
+        this.message = message;
+    }
+
+    public String message(){
+        return this.message;
+    }
 //  state:
 ;
 
