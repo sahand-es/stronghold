e71b6b22f104cccb9bfb73386734cb530cfe3236
sahand-es <844sahand@gmail.com>
2023-05-28 21:30:53 +0330

    main: ProfileViewController: created profile-menu.fxml: logout button added

---
+++ Stronghold/src/main/java/view/fxml/ProfileViewController.java
@@ -0,0 +1,12 @@
+package view.fxml;
+
+import javafx.scene.input.MouseEvent;
+
+public class ProfileViewController {
+
+    public void logout(MouseEvent mouseEvent) {
+    }
+
+    public void deleteAccount(MouseEvent mouseEvent) {
+    }
+}
