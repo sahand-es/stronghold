717091a3da0e882faf4625041a83c59d68cbd17e
Yousef61583 <118717370+Yousef61583@users.noreply.github.com>
2023-05-28 22:07:40 +0330

    main: MainViewController : goToProfileMethod method added

--- Stronghold/src/main/java/view/fxml/MainViewController.java
+++ Stronghold/src/main/java/view/fxml/MainViewController.java
@@ -5,4 +5,8 @@ import javafx.scene.input.MouseEvent;
 public class MainViewController {
     public void startGame(MouseEvent mouseEvent) {
     }
+
+
+    public void goToProfileMenu(MouseEvent mouseEvent) {
+    }
 }
