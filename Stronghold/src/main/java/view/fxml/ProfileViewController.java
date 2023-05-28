cb7441fd854102b36da6fd361d641c0da9e3cad1
sahand-es <844sahand@gmail.com>
2023-05-28 21:32:43 +0330

    main: ProfileViewController:  Fields added profile-menu.fxml: Textfields added

--- Stronghold/src/main/java/view/fxml/ProfileViewController.java
+++ Stronghold/src/main/java/view/fxml/ProfileViewController.java
@@ -1,8 +1,13 @@
 package view.fxml;
 
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class ProfileViewController {
+
+    public TextField oldPass;
+    public TextField newUsername;
+    public TextField newPassword;
 
     public void logout(MouseEvent mouseEvent) {
     }
