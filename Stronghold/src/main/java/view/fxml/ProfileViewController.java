0934df88a010b863fbbccdb8f4a02fb280e35b4b
sahand-es <844sahand@gmail.com>
2023-05-28 21:41:43 +0330

    main: profile-menu.fxml: added label

--- Stronghold/src/main/java/view/fxml/ProfileViewController.java
+++ Stronghold/src/main/java/view/fxml/ProfileViewController.java
@@ -1,6 +1,8 @@
 package view.fxml;
 
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 public class ProfileViewController {
@@ -8,6 +10,11 @@ public class ProfileViewController {
     public TextField oldPass;
     public TextField newUsername;
     public TextField newPassword;
+    public Label usernameLabel;
+    public ImageView rightImage;
+    public ImageView leftImage;
+    public ImageView middleImage;
+    public ImageView mainPicture;
 
     public void logout(MouseEvent mouseEvent) {
     }
