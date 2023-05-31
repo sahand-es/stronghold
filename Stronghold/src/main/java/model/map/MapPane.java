5602de0f9d939ecfecfb737f554968a8f641fcc4
sahand-es <844sahand@gmail.com>
2023-05-31 16:42:57 +0330

    gui-main: Initial isometric map implementation

--- Stronghold/src/main/java/model/map/MapPane.java
+++ Stronghold/src/main/java/model/map/MapPane.java
@@ -1,12 +1,31 @@
 package model.map;
 
+import javafx.scene.Group;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 public class MapPane extends Pane {
-    private static final double WIDTH = 512;
-    private static final double HEIGHT = 512;
+    private static final double WIDTH = 40;
+    private static final double HEIGHT = 40;
     Map map;
+    Group allTiles;
 
+    public MapPane(Map map) {
+        this.map = map;
+        int tilesCount = (int) (WIDTH * HEIGHT);
+        allTiles = new Group();
 
+        for (int i = 0; i < tilesCount; i++) {
+            int x = (int) (i / WIDTH);
+            int y = (int) (i % WIDTH);
 
+            MapTile tile = new MapTile();
+            tile.setTranslateX(MapTile.TILE_WIDTH / 2 * (x-y));
+            tile.setTranslateY(MapTile.TILE_WIDTH / 4 * (x+y));
+            tile.setFill(Color.GREEN);
+
+            allTiles.getChildren().add(tile);
+        }
+        this.getChildren().add(allTiles);
+    }
 }
---
+++ Stronghold/src/main/java/model/map/MapTile.java
@@ -0,0 +1,13 @@
+package model.map;
+
+import javafx.scene.shape.Polygon;
+
+public class MapTile extends Polygon {
+    protected static final double TILE_WIDTH = 32;
+    protected static final double TILE_HEIGHT = 16;
+    private Block block;
+
+    public MapTile() {
+        super( TILE_WIDTH / 2, 0.0, TILE_WIDTH, TILE_HEIGHT / 2, TILE_WIDTH / 2, TILE_HEIGHT, 0.0, TILE_HEIGHT / 2 );
+    }
+}
