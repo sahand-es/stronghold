3142c077542cab9b391449ec8e56729f450ec38b
Yousef61583 <118717370+Yousef61583@users.noreply.github.com>
2023-06-18 19:05:39 +0330

    market-gui: background , label and back button added

--- Stronghold/src/main/java/view/TradeViewController.java
+++ Stronghold/src/main/java/view/TradeViewController.java
@@ -1,16 +1,94 @@
 package view;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import view.shape.ResourceNode;
 
 public class TradeViewController extends Application {
+
+    private BorderPane borderPane;
+
+    private int width;
+    private int height;
 
     public static void main(String[] args) {
         launch();
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage stage) throws Exception {
+        width = (int) Screen.getPrimary().getBounds().getWidth();
+        height = (int) Screen.getPrimary().getBounds().getHeight();
 
+        borderPane = new BorderPane();
+        borderPane.setPrefSize(width, height);
+
+        setBackground();
+        makeTop();
+        makeBottom();
+
+
+
+        Scene scene = new Scene(borderPane);
+        stage.setScene(scene);
+        stage.setFullScreen(true);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    private void setBackground(){
+        Image image = new Image(
+                MarketViewController.class.getResource("/images/backgrounds/background.jpg").toExternalForm()
+        );
+        BackgroundImage backgroundFill = new BackgroundImage(
+                image, BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT
+        );
+        Background background = new Background(backgroundFill);
+        borderPane.setBackground(background);
+    }
+
+    private void makeTop() {
+        Label label = new Label("Trade Menu");
+        label.setAlignment(Pos.CENTER);
+        label.setStyle("-fx-font: 50 sys ; -fx-font-weight: bold");
+        label.setPrefHeight(100);
+
+
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER);
+        hBox.getChildren().add(label);
+        borderPane.setTop(hBox);
+    }
+
+    private void makeBottom() {
+        Button back = new Button("Back");
+        back.setStyle("-fx-font: 20 sys ;-fx-background-color: #e6af29 ; -fx-border-color: #262115");
+        back.setPrefSize(300, 70);
+        back.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                //TODO
+
+            }
+        });
+
+        Label l = new Label();
+        l.setPrefWidth(0);
+        l.setPrefHeight(100);
+
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER);
+        hBox.getChildren().addAll( back,l);
+        borderPane.setBottom(hBox);
     }
 }
