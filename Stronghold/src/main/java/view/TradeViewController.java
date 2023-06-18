b513d5fd8cc243e14e27b72ccfebd0b462f8a600
Yousef61583 <118717370+Yousef61583@users.noreply.github.com>
2023-06-18 18:59:26 +0330

    market-gui: TradeViewController added start Making Trade Menu

---
+++ Stronghold/src/main/java/view/TradeViewController.java
@@ -0,0 +1,16 @@
+package view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TradeViewController extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
