db15398ac3ef61797f14529d1362fec41e346682
ArminGer03 <a1382.geramirad@gmail.com>
2023-06-27 18:49:28 +0330

    Made Login Template fxml

--- Stronghold/src/main/java/view/LoginMenuGUI.java
+++ Stronghold/src/main/java/view/LoginMenuGUI.java
@@ -1,5 +1,37 @@
 package view;
 
-public class LoginMenuGUI {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+import java.net.URL;
+
+public class LoginMenuGUI extends Application {
+
+    public void login(MouseEvent mouseEvent) {
+    }
+
+    public void signUp(MouseEvent mouseEvent) {
+    }
+
+    public void forgotPassword(MouseEvent mouseEvent) {
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL url = LoginMenu.class.getResource("/fxml/login-menu.fxml");
+        assert url != null;
+        BorderPane borderPane = FXMLLoader.load(url);
+        Scene scene = new Scene(borderPane,520,400);
+        primaryStage.setScene(scene);
+        
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
