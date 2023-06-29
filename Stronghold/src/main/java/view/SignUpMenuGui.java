4d8abc3ae46f4bbd44022b6754c19e8fe1190e2a
ArminGer03 <a1382.geramirad@gmail.com>
2023-06-29 08:20:38 +0330

    Added methods to SignUpMenuGui

--- Stronghold/src/main/java/view/SignUpMenuGui.java
+++ Stronghold/src/main/java/view/SignUpMenuGui.java
@@ -1,4 +1,38 @@
 package view;
 
-public class SignUpMenuGui {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class SignUpMenuGui extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL url = LoginMenuGUI.class.getResource("/fxml/signup-menu.fxml");
+        assert url != null;
+        BorderPane borderPane = FXMLLoader.load(url);
+        Scene scene = new Scene(borderPane,1000,600);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void back(ActionEvent actionEvent) {
+    }
+
+    public void signup(ActionEvent actionEvent) {
+    }
+
+    public void resetCaptcha(ActionEvent actionEvent) {
+    }
 }
