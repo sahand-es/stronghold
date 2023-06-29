aa3baa63647fc670555295e63329c8cce470876e
ArminGer03 <a1382.geramirad@gmail.com>
2023-06-29 08:21:19 +0330

    Refactored SignUpMenuGui to SignUpMenuGUI

--- Stronghold/src/main/java/view/SignUpMenuGUI.java
+++ Stronghold/src/main/java/view/SignUpMenuGUI.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 
 import java.net.URL;
 
-public class SignUpMenuGui extends Application {
+public class SignUpMenuGUI extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
