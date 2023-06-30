da27dd072dbdf096570f093312c8c64a2be7012c
Yousef61583 <118717370+Yousef61583@users.noreply.github.com>
2023-06-30 21:27:54 +0330

    network-main: reconnecting to server tested

--- Stronghold/src/main/java/model/App.java
+++ Stronghold/src/main/java/model/App.java
@@ -2,6 +2,27 @@ package model;
 
 import javafx.stage.Stage;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class App {
     public static Stage stage;
+
+    private static Socket socket;
+
+    private static DataInputStream dataInputStream;
+    private static DataOutputStream dataOutputStream;
+
+    static {
+        try {
+            socket = new Socket("localhost",8000);
+            dataOutputStream = new DataOutputStream (socket.getOutputStream());
+            dataInputStream = new DataInputStream (socket.getInputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
--- Stronghold/src/main/java/model/network/ClientTest.java
+++ Stronghold/src/main/java/model/network/ClientTest.java
@@ -3,23 +3,39 @@ package model.network;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.ConnectException;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class ClientTest {
+    static DataOutputStream dataOutputStream;
+    static DataInputStream dataInputStream;
 
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost",8000);
-        DataOutputStream dataOutputStream = new DataOutputStream (socket.getOutputStream());
-        DataInputStream dataInputStream = new DataInputStream (socket.getInputStream());
         Scanner scanner = new Scanner(System.in);
-        dataOutputStream.writeUTF("1");
-        System.out.println(dataInputStream.readUTF());
+        Socket socket = connect(scanner);
+        dataOutputStream = new DataOutputStream (socket.getOutputStream());
+        dataInputStream = new DataInputStream (socket.getInputStream());
 
         while (true){
             String message = scanner.nextLine();
             dataOutputStream.writeUTF(message);
             System.out.println(dataInputStream.readUTF());
         }
+    }
+
+    private static Socket connect(Scanner scanner) {
+        Socket socket;
+        try {
+            socket = new Socket("localhost",8000);
+        } catch (ConnectException e){
+            System.out.println("no server found enter something to reconnect");
+
+            while (!scanner.nextLine().equals("reconnect")) System.out.println("no");
+            socket = connect(scanner);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return  socket;
     }
 }
