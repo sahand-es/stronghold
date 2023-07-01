21f9a8d432bad393c0e0e376a4ebe740d4915880
ArminGer03 <a1382.geramirad@gmail.com>
2023-07-01 16:12:20 +0330

    Added all the todos related to lobby

--- Stronghold/src/main/java/view/GUIController/LobbyGUI.java
+++ Stronghold/src/main/java/view/GUIController/LobbyGUI.java
@@ -11,8 +11,10 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import view.shape.lobby.GameSessionNode;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class LobbyGUI extends Application {
@@ -23,11 +25,17 @@ public class LobbyGUI extends Application {
     @FXML
     private ChoiceBox playerNumberChoice;
 
+    private ArrayList<HBox> gameSessionMainNodes = new ArrayList<>();
+
+
     ObservableList<String> numberList = FXCollections.observableArrayList("2","3","4","5","6","7","8");
 
     @FXML
     public void initialize() {
+        //todo ongoing sessions must be added to gameSessionMainNodes ArrayList inorder to be shown in lobby
+
         playerNumberChoice.setItems(numberList);
+        playerNumberChoice.setValue(numberList.get(0));
 
         //load background
         Image image = new Image(
@@ -38,14 +46,29 @@ public class LobbyGUI extends Application {
                 BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,BackgroundSize.DEFAULT);
         Background background = new Background(backgroundFill);
         anchorPane.setBackground(background);
+
+
+
     }
 
     public void createGame() {
+        //todo check whether if the user is joined in another match
+        //todo make a new Game
+        //todo make a new GameSession node via this line of code:
+
+        HBox newSession = new GameSessionNode().getMainNode();
+        gameSessionMainNodes.add(newSession);
+        VBox.getChildren().add(newSession);
+
+    }
+
+    public void refresh() {
+        //todo remove all the nodes from ArrayList and make them all over again the process of making a new node will refresh all its details
 
     }
 
     public void back() {
-
+        //todo go to previous menu
     }
 
 
@@ -54,8 +77,8 @@ public class LobbyGUI extends Application {
     public void start(Stage primaryStage) throws Exception {
         URL url = LoginMenuGUI.class.getResource("/fxml/lobby.fxml");
         assert url != null;
-        BorderPane borderPane = FXMLLoader.load(url);
-        Scene scene = new Scene(borderPane,1000,600);
+        AnchorPane aPane = FXMLLoader.load(url);
+        Scene scene = new Scene(aPane,1000,600);
         primaryStage.setScene(scene);
         primaryStage.setFullScreen(true);
         primaryStage.show();
@@ -65,5 +88,6 @@ public class LobbyGUI extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
 
 }
--- Stronghold/src/main/java/view/shape/lobby/GameSessionNode.java
+++ Stronghold/src/main/java/view/shape/lobby/GameSessionNode.java
@@ -1,10 +1,12 @@
 package view.shape.lobby;
 
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import model.Game;
 import utility.RandomGenerators;
 
@@ -17,43 +19,48 @@ public class GameSessionNode {
     private Button leave;
     private Button start;
 
-    public GameSessionNode(Game game) {
-        this.game = game;
+    //todo constructor must get game and the user that wants the node to be made
+    public GameSessionNode() {
+        //this.game = game;
         mainNode  = new HBox();
         mainNode.setAlignment(Pos.CENTER);
         mainNode.setSpacing(100);
-        mainNode.setPrefSize(600,70);
+        mainNode.setPrefSize(1300,100);
         mainNode.setStyle("-fx-background-color: #263f73");
 
         Label idLabel = new Label("Session Id: " + RandomGenerators.randomSessionId());
-        joinedNumberLabel.setPrefSize(100,70);
-        idLabel.setStyle("-fx-font: 20 sys");
+        idLabel.setPrefSize(150,100);
+        idLabel.setStyle("-fx-font: 18 sys");
+        idLabel.setTextFill(Color.WHITE);
         mainNode.getChildren().add(idLabel);
 
         usersArea = new TextArea();
         usersArea.setEditable(false);
-        usersArea.setPrefSize(200,70);
+        usersArea.setPrefSize(300,100);
         usersArea.setStyle("-fx-font: 20 sys");
-        //todo get usernames from game
+        //todo get usernames from game(from constructor)
         usersArea.setText("Test mikonim \nbebinim\nche\nkhabare\nshayad\njaleb shod yeho khoshemoon oomad");
         mainNode.getChildren().add(usersArea);
 
-        //todo set count by game
+        //todo set players count by game(from constructor)
         joinedNumberLabel = new Label("Players count: 1/5");
-        joinedNumberLabel.setPrefSize(100,70);
-        joinedNumberLabel.setStyle("-fx-font: 20 sys");
+        joinedNumberLabel.setPrefSize(150,70);
+        joinedNumberLabel.setStyle("-fx-font: 18 sys");
+        joinedNumberLabel.setTextFill(Color.WHITE);
         mainNode.getChildren().add(joinedNumberLabel);
 
 
+        //todo it must be checked if the user is joined in this match or not to set the button visibility
         join = new Button("Join");
         join.setStyle("-fx-font: 20 sys");
         mainNode.getChildren().add(join);
 
-
+        //todo it must be checked if the user is joined in this match or not to set the button visibility
         leave = new Button("Leave");
         leave.setStyle("-fx-font: 20 sys");
         mainNode.getChildren().add(leave);
 
+        //todo it must be checked if the user is joined in this match or not to set the button visibility
         start = new Button("Start");
         start.setStyle("-fx-font: 20 sys");
         mainNode.getChildren().add(start);
@@ -81,5 +88,9 @@ public class GameSessionNode {
 
     public Button getStart() {
         return start;
+    }
+
+    public Game getGame() {
+        return game;
     }
 }
